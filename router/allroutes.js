5b2c55f8c38e226ca324e38fa08798656338c841
Swaroopgophane <swaroop.gophane2017@gmail.com>
2023-03-20 14:51:26 +0530

    display changes

--- router/allroutes.js
+++ router/allroutes.js
@@ -79,6 +79,7 @@ router.post('/signin', async (req,res) =>{
     
                 const token = await userLogin.generateToken();
                 
+                console.log(token);
     
                 res.cookie("rwtoken",token,{
                     httpOnly: true,
